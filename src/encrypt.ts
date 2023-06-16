964ae3850405f1fdc49039b63b5917ce45c5520b
wighawag <wighawag@gmail.com>
2023-06-16 10:00:30 +0100

    log

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -1,8 +1,9 @@
 import { encryptMessageForXRounds } from "./tlock"
 
 export function setupEncryptButton(element: HTMLButtonElement) {
-  function encrypt() {
-    encryptMessageForXRounds('hello', 4);
+  async function encrypt() {
+    const encrypted = await encryptMessageForXRounds('hello', 4);
+    console.log(encrypted)
   }
   element.addEventListener('click', () => encrypt())
 }
